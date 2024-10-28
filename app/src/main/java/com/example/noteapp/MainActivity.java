b97b4211e1099800e0bca63bf4b77a577568a975
Alumnos <nicolas.lagos.acevedo1@gmail.com>
2024-10-28 10:47:02 -0300

    Agregue un recyclerview y definí variables

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.noteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Struct;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> alista = new ArrayList<String>();
+    RecyclerView lista;
+    EditText nota;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nota = findViewById(R.id.editTextTextPersonName);
+        lista = findViewById(R.id.Notas);
     }
 }
 //HOla Conny
